0092139969791c0962e3ddfb31c0ba01b680d469
KENT <kwilliam866@gmail.com>
2024-07-26 23:55:17 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Testing the function
+console.log(saturdayFun()); 
+console.log(saturdayFun("dance"));
+
+function mondayWork(activity ="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork());
+console.log(mondayWork("go to the gym"));
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
